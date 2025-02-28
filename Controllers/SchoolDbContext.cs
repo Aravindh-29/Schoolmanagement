ffef8c7edd02ad1bbb2a0c8bdf6de9a76e300aa5
Aravindh-29 <aravindhkumar146@gmail.com>
2025-02-28 12:25:57 +0530

    fc

---
+++ Controllers/SchoolDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolManagement.Models;
+
+namespace SchoolManagement.Controllers
+{
+    public class SchoolDbContext:DbContext
+    {
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options):base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Ensure table is created and seeded with initial data
+            modelBuilder.Entity<Student>().HasData(
+                new Student { Id = 1, Name = "John Doe", Age = 14, Class = "8th Grade" },
+                new Student { Id = 2, Name = "Jane Smith", Age = 13, Class = "7th Grade" }
+            );
+        }
+    }
+    
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolManagement.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly SchoolDbContext _context;
+        public StudentController(SchoolDbContext context) {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var students = _context.Students.ToList();
+            return View(students);
+        }
+    }
+}
